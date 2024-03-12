0700b4e9b925374edeff49cad88453b654b08251
YUBIN CHOE <cybin050300@naver.com>
2024-03-12 19:44:29 +0900

    emp 연습

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7c/2000ec805be0001e1346be5fc94f2cdd
@@ -0,0 +1,9 @@
+package com.cyb.dto;
+
+public class Employee {
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- java_mysql_emp/src/com/cyb/dto/Employee.java
+++ java_mysql_emp/src/com/cyb/dto/Employee.java
@@ -1,9 +1,17 @@
 package com.cyb.dto;
 
 public class Employee {
+	private int empNo = 0; // 사원 고유번호
+	private String empName = ""; // 사원이름
+	private String job = ""; // 직급
+	private String mgr = ""; // 직급번호
+	private String hireDate = ""; // 입사일
+	private int sale = 0; // 급여
+	private int commission = 0; // 성과급
+	private int deptNo = 0; // 부서번호
 
 	public Employee() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
